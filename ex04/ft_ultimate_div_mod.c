b3cd560b1a3e948083719adf6ffd84e3280121b1
Siyabonga <smzimela@student.wethinkcode.co.za>
2020-06-25 11:11:17 +0200

    done

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -2,9 +2,15 @@
 
 void ft_ultimate_div_mode(int *a , int *b)
 {
-	*a = &a / &b;
-	*b = &a % &b;
-}
+	int x = *a; 
+	int y = *b;
 
+	int p = x / y;
+	int q = x % y;
+
+	*a = &p;
+	*b = &q;
+}
+	
 
 
